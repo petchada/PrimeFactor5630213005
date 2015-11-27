84761afa5966420bf27e6845f449f27cb1d94d07
TukTik <petchadazii@gmail.com>
2015-11-27 18:03:37 +0700

    CREATE NEW FILE NAME PrimeFactor5630213005.java

---
+++ PrimeFactor5630213005.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primefactor5630213005;
+
+/**
+ *
+ * @author STD-PC
+ */
+public class PrimeFactor5630213005 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
