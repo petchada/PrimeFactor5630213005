c1594d225581887d570b4d14b86f505775d69d5d
petchada <petchadazii@gmail.com>
2015-11-27 18:13:55 +0700

    Update PrimeFactor5630213005.java

--- PrimeFactor5630213005.java
+++ PrimeFactor5630213005.java
@@ -1,21 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package primefactor5630213005;
 
-/**
- *
- * @author STD-PC
- */
+import java.util.Scanner;
+
 public class PrimeFactor5630213005 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public static void main(String[] args) {
+Scanner number = new Scanner(System.in);
+
+int i =1;
+while(i==1){
+System.out.print("Input a number: ");
+int numbers = number.nextInt();
+int primeFactor=2;
+
+while(numbers>1){
+if(numbers%primeFactor == 0) {
+System.out.println(primeFactor+" ");
+numbers=numbers/primeFactor;
+}
+else
+primeFactor++;
+}
+}
+System.out.println(" ");
+}
+
 }
